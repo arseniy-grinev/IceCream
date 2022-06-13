6353942384a911cd57b7d377f67bbf63fef73660
yerma4ok <yerma4ok13@gmail.com>
2022-06-13 17:24:58 +0300

    init bugfix\contacts-loc-mod

--- src/js/modal-contacts.js
+++ src/js/modal-contacts.js
@@ -1,17 +1,18 @@
 (() => {
-   const refs = {
-     openModalBtn: document.querySelector("[data-location-open]"),
-     closeModalBtn: document.querySelector("[data-location-close]"),
-     modal: document.querySelector("[data-location]"),
-   };
- 
-   refs.openModalBtn.addEventListener("click", toggleModal);
-   refs.closeModalBtn.addEventListener("click", toggleModal);
- 
-   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    if (!window.menuIsOpened) {
-      refs.body.classList.toggle("no-scroll");
-    }
-  }
- })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-location-open]"),
+    closeModalBtn: document.querySelector("[data-location-close]"),
+    modal: document.querySelector("[data-location]"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+   refs.modal.classList.toggle("is-hidden");
+   if (!window.menuIsOpened) {
+     refs.body.classList.toggle("no-scroll");
+   }
+ }
+})();
